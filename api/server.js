b4a977a3b6d24c1eb73fcf2ed73f642022bbc52a
dislersd <dislersd@gmail.com>
2019-04-02 10:21:10 -0700

    updated restriced function to use session

--- api/server.js
+++ api/server.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+// STEP 2 bring in library
+const session = require('express-session')
+// Step 5 bring in connect session store and pass it session
+const KnexSessionStore = require('connect-session-knex')(session);
+
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const configureKnex = require('../database/dbConfig')
 
 const server = express();
+
+//STEP 2.2 Set up session config
+const sessionConfig = {
+  name: 'monster',
+  secret: 'keep it secret, keep it safe!',
+  cookie: {
+    maxAge: 1000 * 60 * 10, // milliseconds
+    secure: false,// use cookie over https
+    httpOnly: true,// false means js can access the cookie on client
+  },
+  resave: false,// avoid recreating unchanged sessions
+  saveUninitialized: false,// GDPR compliance - READ ABOUT THIS
+  // STEP 5.1 set up store
+  store: new KnexSessionStore({
+    knex: configureKnex,
+  })
+}
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+//STEP 2.1 use session on server
+server.use(session(sessionConfig))
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -25,6 +25,9 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // STEP 3 req.session is added by express-session
+        req.session.user = user;
+        
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
